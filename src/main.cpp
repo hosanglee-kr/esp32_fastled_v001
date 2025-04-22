d7255c113f0d537c572761147a2191d99e9c072e
hosanglee-kr <ddongl@gmail.com>
2025-04-22 09:22:52 +0900

    aaa

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,27 @@
 
 // The build version comes from an environment variable. Use the VERSION
 // define wherever the version is needed.
-#define STRINGIZER(arg) #arg
-#define STR_VALUE(arg)	STRINGIZER(arg)
-#define VERSION			STR_VALUE(BUILD_VERSION)
+
 
 #define S10
 #ifdef S10
-	#include "S10_streams-i2s-webserver_wav_001.h"
+	#include "S10.h"
 #endif
 
 void setup() {
-	// delay(5000);
 
 	Serial.begin(115200);
-	Serial.print("Version: ");
-	Serial.println(VERSION);
 
-#ifdef S10
-	S10_setup();
-#endif
+
+	#ifdef S10
+		S10_setup();
+	#endif
 
 	Serial.println("11111");
 }
 
 void loop() {
-#ifdef S10
-	S10_loop();
-#endif
+	#ifdef S10
+		S10_loop();
+	#endif
 }
