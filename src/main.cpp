a121e526068c7c4de0e0f2e465f1f329628d89ce
hosanglee-kr <ddongl@gmail.com>
2025-04-22 19:16:30 +0900

    add F120, F130 examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 
-
-#define F100
+//#define F100
 #ifdef F100
 	#include "F100_fxWave2D_002.h"
 #endif
 	
 //#define F110
 #ifdef F110
-	#include "F110_wave2D_001.h"
+	#include "F110_wave2D_002.h"
+#endif
 
+
+//#define F120
+#ifdef F120
+	#include "F120_Blur2d_001.h"
+#endif
+
+#define F130
+#ifdef F130
+	#include "F130_Wave2d_001.h"
 #endif
 
 void setup() {
@@ -24,7 +33,14 @@ void setup() {
 	     F110_init();
     #endif
 
+	#ifdef F120
+		F120_init();
+	#endif
 
+	#ifdef F130
+		F130_init();
+	#endif
+	
 	Serial.println("11111");
 }
 
@@ -37,4 +53,11 @@ void loop() {
 	     F110_run();
     #endif
 
+    #ifdef F120
+	     F120_run();
+    #endif
+
+    #ifdef F130
+	     F130_run();
+    #endif
 }
